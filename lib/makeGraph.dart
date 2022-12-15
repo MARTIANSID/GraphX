6e5f5c9cda516a83039b7d80ab38dba4372fbca3
Yash Gupta <73610050+YashJA-dev@users.noreply.github.com>
2022-12-15 16:05:50 +0530

    node ui updated

--- lib/makeGraph.dart
+++ lib/makeGraph.dart
@@ -9,21 +9,30 @@ class MakeGraph extends CustomPainter {
   MakeGraph({required this.nodes, required this.edges});
   @override
   void paint(Canvas canvas, Size size) {
-    var paint1 = Paint()
-      ..color = Colors.lightBlue
+    var nodeInnerPaint = Paint()
+      ..color = Color.fromRGBO(104, 174, 186, 1)
       ..style = PaintingStyle.fill;
-
+    var nodeOutterPaint= Paint()
+      ..color = Color.fromRGBO(168, 180, 195, 1)
+      ..strokeWidth=2
+      ..style = PaintingStyle.stroke;
+      
+    double nodedRadius=18;
     for (int i = 0; i < nodes.length; i++) {
+      String nodeName="${i+1}";
       TextSpan span =
-          TextSpan(style: TextStyle(color: Colors.black), text: "${i + 1}");
+          TextSpan(style: TextStyle(color: Colors.yellow), text: nodeName);
       TextPainter textPainter = TextPainter(
           text: span,
           textAlign: TextAlign.center,
           textDirection: TextDirection.ltr);
       textPainter.layout();
-      canvas.drawCircle(nodes[i].coordinates!, 10, paint1);
-      textPainter.paint(canvas,
-          Offset(nodes[i].coordinates!.dx - 5, nodes[i].coordinates!.dy - 5));
+      //inner color of node
+      canvas.drawCircle(nodes[i].coordinates!, nodedRadius, nodeInnerPaint);
+      //boundary of a node
+      canvas.drawCircle(nodes[i].coordinates!, nodedRadius, nodeOutterPaint);
+      //node number
+      textPainter.paint(canvas,getTextPaintCordinates(coordinates: nodes[i]!.coordinates!,nodeName: nodeName));
     }
     for (int i = 0; i < edges.length; i++) {
       Edge e = edges[i];
@@ -40,7 +49,14 @@ class MakeGraph extends CustomPainter {
   bool shouldRepaint(MakeGraph oldDelegate) {
     return true;
   }
-
+  Offset getTextPaintCordinates({required Offset coordinates,required String nodeName}){
+    if(nodeName.length==1){
+      return Offset(coordinates.dx-5, coordinates.dy-5);
+    }else if(nodeName.length==2){
+       return Offset(coordinates.dx-9, coordinates.dy-5);
+    }
+     return Offset(coordinates.dx-13, coordinates.dy-5);;
+  }
   @override
   bool shouldRebuildSemantics(MakeGraph oldDelegate) => false;
 }
